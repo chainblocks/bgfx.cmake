e12f5717937fb07502fbe295730adbc6f414cf58
Guus Waals <_@guusw.nl>
2021-08-03 14:21:33 +0200

    Make tinyexr and interface library

--- cmake/3rdparty/tinyexr.cmake
+++ cmake/3rdparty/tinyexr.cmake
@@ -4,6 +4,6 @@ endif()
 
 file( GLOB_RECURSE TINYEXR_SOURCES ${BIMG_DIR}/3rdparty/tinyexr/*.c ${BIMG_DIR}/3rdparty/tinyexr/*.h )
 
-add_library( tinyexr STATIC ${TINYEXR_SOURCES} )
-target_include_directories( tinyexr PUBLIC $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty> $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty/tinyexr/deps/miniz> )
+add_library( tinyexr INTERFACE ${TINYEXR_SOURCES} )
+target_include_directories( tinyexr INTERFACE $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty> $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty/tinyexr/deps/miniz> )
 set_target_properties( tinyexr PROPERTIES FOLDER "bgfx/3rdparty" )
