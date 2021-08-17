35ec0e547e226c022e9ddc7bf8160f133def9b58
Guus Waals <_@guusw.nl>
2021-08-17 20:04:33 +0200

    Remove tinyexr sources from interface library

--- cmake/3rdparty/tinyexr.cmake
+++ cmake/3rdparty/tinyexr.cmake
@@ -2,8 +2,6 @@ if( TARGET tinyexr )
 	return()
 endif()
 
-file( GLOB_RECURSE TINYEXR_SOURCES ${BIMG_DIR}/3rdparty/tinyexr/*.c ${BIMG_DIR}/3rdparty/tinyexr/*.h )
-
-add_library( tinyexr INTERFACE ${TINYEXR_SOURCES} )
+add_library( tinyexr INTERFACE )
 target_include_directories( tinyexr INTERFACE $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty> $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty/tinyexr/deps/miniz> )
 set_target_properties( tinyexr PROPERTIES FOLDER "bgfx/3rdparty" )
