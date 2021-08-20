e636fbbf1e378377a1a6db07db19675e100369e5
Guus Waals <_@guusw.nl>
2021-08-20 16:21:26 +0200

    Remove PROPERTIES FOLDER from tinyexr interface library

--- cmake/3rdparty/tinyexr.cmake
+++ cmake/3rdparty/tinyexr.cmake
@@ -4,4 +4,3 @@ endif()
 
 add_library( tinyexr INTERFACE )
 target_include_directories( tinyexr INTERFACE $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty> $<BUILD_INTERFACE:${BIMG_DIR}/3rdparty/tinyexr/deps/miniz> )
-set_target_properties( tinyexr PROPERTIES FOLDER "bgfx/3rdparty" )
